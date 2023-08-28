2d12d43703bf46e0a47db399b41aa21876048983
Diego-Bonora <bonoradi@gmail.com>
2023-08-28 06:08:37 -0700

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" typed anotated function that adds 2 floats"""
+""" typed anotated function that adds 2 floats """
 
 
 def add(a: float, b: float) -> float:
+    """ returns the addition of a + b """
     return a + b
