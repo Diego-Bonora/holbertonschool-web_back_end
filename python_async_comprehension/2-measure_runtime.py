b75280ddcba7e2f4f6625e9cff961a01cf3dcd80
Diego-Bonora <bonoradi@gmail.com>
2023-08-29 10:57:20 -0700

    small fix

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """ calculates the time it takes to execute 4 coroutines in parallel """
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
     end = time.time()
     total_time = end - start
     return total_time
