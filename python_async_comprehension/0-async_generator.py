8a01eb049b3cada082e573be1aee551d379cfc11
Diego-Bonora <bonoradi@gmail.com>
2023-08-29 10:17:43 -0700

    added annotations

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ yield a random number """
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float]:
     """ yield a random number """
     for i in range(10):
         await asyncio.sleep(1)
