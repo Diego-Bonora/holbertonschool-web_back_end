975847918c23e1414f816165aace5f1f1619a252
Diego-Bonora <bonoradi@gmail.com>
2023-10-10 07:32:16 -0700

    fix

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 export default function getStudentsByLocation(getListStudents, city) {
   let locationArray = [];
   if (getListStudents instanceof Array) {
-    locationArray = getListStudents.filter((item) => item.location == city);
+    locationArray = getListStudents.filter((item) => item.location === city);
   }
   return locationArray;
 }
